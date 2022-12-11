aef264d8e14d5e5172790c7838a2e37f4a35d8c5
Maxime Dupont <emixamdupont@gmail.com>
2022-12-11 20:22:18 +0100

    remove console.log

--- scripts/generate.mjs
+++ scripts/generate.mjs
@@ -1,14 +1,11 @@
 import config from '../packages/static/src/generateConfig.mjs';
 import path from 'path';
 import fs from 'fs';
-import { appendFile, getFilename, makeDir, parseArgs, resetFile } from './helpers.mjs';
+import { appendFile, getFilename, resetFile } from './helpers.mjs';
 
 const templateDir = './templates';
 const templates = fs.readdirSync(templateDir);
-// const index =  "index.ts.js";
 const dist = './src/lib/icons';
-
-console.log(templates);
 
 const templateFiles = templates.map((template) =>
 	import('file://' + path.resolve(process.cwd(), templateDir, template))
@@ -16,7 +13,6 @@ const templateFiles = templates.map((template) =>
 const generators = await Promise.all(templateFiles).then((res) =>
 	res.map((v, i) => ({ filename: getFilename(templates[i]), m: v.default }))
 );
-console.log(JSON.stringify({ l: config.length, config }));
 
 config.forEach((app) => {
 	generators.forEach((generator) => {
@@ -26,11 +22,3 @@ config.forEach((app) => {
 		appendFile(filePath, generator.m({ app, config }));
 	});
 });
-
-// if (templates.findIndex(t => t === index) >= 0) {
-//     const indexGenerator = await import("file://" + path.resolve(process.cwd(), templateDir, index))
-//     const folderPath = path.resolve(process.cwd(), dist);
-//     const filePath = path.resolve(folderPath, getFilename(index));
-//     resetFile(filePath);
-//     appendFile(filePath, indexGenerator.default(config))
-// }
