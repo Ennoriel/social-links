3a9528248c2bb25d9aca8ae0b1bfba1b76ad0477
Maxime Dupont <emixamdupont@gmail.com>
2022-12-31 05:37:50 +0100

    up

--- packages/svelte/src/lib/Link.svelte
+++ packages/svelte/src/lib/Link.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import Icon from '$lib/Icon.svelte';
-	import { getUrl, type Config } from 'static-link-share';
+	import { getUrl, type Config } from 'static-social-share';
 
 	export let config: Config;
 
