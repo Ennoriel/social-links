ee96a8fe65f2d31ae97d577cb81f90a18c5350f9
Maxime Dupont <emixamdupont@gmail.com>
2023-01-19 09:48:53 +0100

    Add favicon

---
+++ packages/site/src/routes/icon/+page.svelte
@@ -0,0 +1,34 @@
+<script>
+	import { FacebookLink, HackerNewsLink, LinkedInLink, RedditLink } from 'svelte-social-link';
+</script>
+
+<div class="page">
+    <div class="front">
+        <FacebookLink/>
+        <HackerNewsLink/>
+        <LinkedInLink/>
+        <RedditLink/>
+    </div>
+</div>
+
+<style>
+    .page {
+        position: absolute;
+        inset: 0;
+        background: #eee;
+    }
+
+    .front {
+        position: absolute;
+        top: 50%;
+        left: 50%;
+        transform: translate(-50%, -50%);
+
+        display: inline-block;
+        padding: 2px;
+        height: 68px;
+        width: 68px;
+        background: white;
+        box-shadow: 0 0 64px 8px #999;
+    }
+</style>
