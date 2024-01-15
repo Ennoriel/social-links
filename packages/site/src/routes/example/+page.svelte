0b59c105761ef7da344abb5bcbcd5d40e2eb7102
Maxime Dupont <m.dupont103@gmail.com>
2024-01-15 22:43:53 +0100

    update preview images

--- packages/site/src/routes/example/+page.svelte
+++ packages/site/src/routes/example/+page.svelte
@@ -1,34 +1,71 @@
-<script>
+<script lang="ts">
 	import AllLinks from '$lib/AllLinks.svelte';
+
+	let size: 'sm' | 'md' | 'lg' = 'md';
 </script>
 
-<div class="page">
-    <div class="front">
-        <AllLinks/>
+<div class="wrapper">
+	<input type="radio" value="sm" bind:group={size} />
+	<input type="radio" value="md" bind:group={size} />
+	<input type="radio" value="lg" bind:group={size} />
 
-        <AllLinks round/>
+	<div class="page {size}">
+		<div class="front">
+			<AllLinks size={size === 'lg' ? 64 : 32} />
 
-        <AllLinks color="#123456"/>
-    </div>
+			<AllLinks size={size === 'lg' ? 64 : 32} round />
+
+			<AllLinks size={size === 'lg' ? 64 : 32} color="#123456" />
+		</div>
+	</div>
 </div>
 
 <style>
-    .page {
-        position: absolute;
-        inset: 0;
-        background: #eee;
-    }
+	.wrapper {
+		position: absolute;
+		inset: 0;
+		background: #eee;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
 
-    .front {
-        position: absolute;
-        top: 50%;
-        left: 50%;
-        transform: translate(-50%, -50%);
+	.page.sm {
+		width: 640px;
+		height: 320px;
+	}
 
-        display: inline-block;
-        padding: 32px;
-        border-radius: 16px;
-        background: white;
-        box-shadow: 0 0 64px 8px #999;
-    }
+	.page.md {
+		width: 1280px;
+		height: 320px;
+	}
+
+	.page.lg {
+		width: 1280px;
+		height: 640px;
+	}
+
+	.page .front :global(div) {
+		gap: 8px;
+		margin: 4px 0;
+	}
+
+	.page.lg .front :global(div) {
+		gap: 16px;
+		margin: 8px 0;
+	}
+
+	.front {
+		position: absolute;
+		top: 50%;
+		left: 50%;
+		transform: translate(-50%, -50%);
+
+		display: flex;
+		flex-direction: column;
+		padding: 32px;
+		border-radius: 16px;
+		background: white;
+		box-shadow: 0 0 64px 8px #999;
+	}
 </style>
